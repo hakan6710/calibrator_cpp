36447b91a0f7ae15a5d448036cb5b8b3156e0a20
Hakan Köse <hakan.koese@alumni.fh-aachen.de>
2021-09-20 16:53:56 +0200

    first commit

---
+++ build/CMakeFiles/ros2Mot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros2Mot.dir/main.cpp.o"
+  "ros2Mot.pdb"
+  "ros2Mot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ros2Mot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
